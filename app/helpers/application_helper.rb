50aa21cccef1d08fbf7d305ac2f7864c80fdf10b
jfagan132 <jfagan@ggc.edu>
2015-11-30 15:25:33 +0000

    added routes for about and help, and added new styling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def full_title(page_title = '')
-        base_title = "GameRecc"
+        base_title = "Game-Recc"
         if page_title.empty?
             base_title
         else
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  get 'home/index'
   root 'home#index'
+  get 'home#about'
+  get 'home#help'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
